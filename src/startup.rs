4585318fda261a1bada25ea4a7a3ebb053e47e5e
和兑 <chenzhangchao@cai-inc.com>
2023-03-06 20:25:01 +0800

    feat: .env feat: add postgres connection state fix: general.yml

--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,20 @@
 use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server;
 use std::net::TcpListener;
+use sqlx::PgConnection;
 
 use crate::routes;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    let connection = web::Data::new(connection);
+    // Capture `connection` from the surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
+            // Get a pointer copy and attach it to the application state
+            .app_data(connection.clone())
     })
         .listen(listener)?
         .run();
