3bacec281a7ecf6a6daac7b5f9aacc133cbb5b10
和兑 <chenzhangchao@cai-inc.com>
2023-03-16 15:35:08 +0800

    fix: SubscriberName Result

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -21,11 +21,15 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().finish()
+    };
     // `web::Form` is a wrapper around `FormData`
     // `form.0` gives us access to the underlying `FormData`
     let new_subscriber = NewSubscriber {
         email: form.0.email,
-        name: SubscriberName::parse(form.0.name).expect("Name validation failed."),
+        name,
     };
 
     match insert_subscriber(&pool, &new_subscriber).await {
