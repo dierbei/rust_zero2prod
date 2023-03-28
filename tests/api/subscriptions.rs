d77bf96275d15d75a7b072ffd4fff96aeb88e768
和兑 <chenzhangchao@cai-inc.com>
2023-03-28 16:33:37 +0800

    fix: add confirm email unit test

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -3,6 +3,43 @@ use wiremock::{Mock, ResponseTemplate};
 use crate::helpers::spawn_app;
 
 #[actix_rt::test]
+async fn subscribe_sends_a_confirmation_email_with_a_link() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        // We are not setting an expectation here anymore
+        // The test is focused on another aspect of the app
+        // behaviour.
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+
+    // Get the first intercepted request
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    // Parse the body as JSON, starting from raw bytes
+    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+
+    let get_link = |s: &str| {
+        let links: Vec<_> = linkify::LinkFinder::new()
+            .links(s)
+            .filter(|l| *l.kind() == linkify::LinkKind::Url)
+            .collect();
+
+        assert_eq!(links.len(), 1);
+        links[0].as_str().to_owned()
+    };
+
+    let html_link = get_link(&body["HtmlBody"].as_str().unwrap());
+    let text_link = get_link(&body["TextBody"].as_str().unwrap());
+    // The two links should be identical
+    assert_eq!(html_link, text_link);
+}
+
+#[actix_rt::test]
 async fn subscribe_send_a_confirmation_email_for_valid_data() {
     // Arrange
     let app = spawn_app().await;
